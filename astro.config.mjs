ea4f259b48e8677495ac5d9bd757bdbbe31e6456
SAIF OMRAN <saifomran262977@gmail.com>
2026-09-25 22:23:09 +0300

    Update astro.config.mjs
    remove sitemap temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://saleel-alsawarim.pages.dev',
@@ -18,7 +17,6 @@ export default defineConfig({
 
   integrations: [
     mdx(),
-    sitemap(),
   ],
 
   server: {
